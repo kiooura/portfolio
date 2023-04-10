fa91e59e08b23a0f4f899464c2fd1c102760e9ef
Koura5 <32008@snu.ac.kr>
2023-04-10 12:17:14 +0300

    some randaon java

---
+++ assets/javaScript.js
@@ -0,0 +1,61 @@
+
+/*
+
+window.addEventListener("animationend", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("animationend", () => {
+    document.body.removeChild(loader);
+  });
+});
+
+
+*/
+
+
+
+
+$(document).ready(function() {
+
+  // required elements
+  var imgPopup = $('.img-popup');
+  var imgCont  = $('.container__img-holder');
+  var popupImage = $('.img-popup img');
+  var closeBtn = $('.close-btn');
+
+  // handle events
+  imgCont.on('click', function() {
+    var img_src = $(this).children('img').attr('src');
+    imgPopup.children('img').attr('src', img_src);
+    imgPopup.addClass('opened');
+  });
+
+  $(imgPopup, closeBtn).on('click', function() {
+    imgPopup.removeClass('opened');
+    imgPopup.children('img').attr('src', '');
+  });
+
+  popupImage.on('click', function(e) {
+    e.stopPropagation();
+  });
+  
+});
+
+
+
+$(function () {
+  "use strict";
+  
+  $(".popup img").click(function () {
+      var $src = $(this).attr("src");
+      $(".show").fadeIn();
+      $(".img-show img").attr("src", $src);
+  });
+  
+  $("span, .overlay").click(function () {
+      $(".show").fadeOut();
+  });
+  
+});
