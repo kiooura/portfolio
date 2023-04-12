c76695d5ca9ae9770c377caca1c2e8f7a55bdb23
Koura5 <32008@snu.ac.kr>
2023-04-13 01:23:09 +0300

    add minor changes

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -59,3 +59,18 @@ $(function () {
   });
   
 });
+
+
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+
+
+
+
